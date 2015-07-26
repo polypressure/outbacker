e873faa31112f44185a368c926d461cc90441e00
Anthony Garcia <polypressure@outlook.com>
2015-07-26 18:48:15 -0500

    Fix Coveralls config.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,18 @@
-require 'coveralls'
-Coveralls.wear!
+if ENV["TRAVIS"]
+  require "simplecov"
+  require "coveralls"
+
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  SimpleCov.start do
+    # No need to report coverage metrics for the test code
+    add_filter "test"
+  end
+
+  # Eager load the entire lib directory so that SimpleCov is able to report
+  # accurate code coverage metrics.
+  outbacker_lib = File.expand_path("../../../lib", __FILE__)
+  at_exit { Dir["#{outbacker}/**/*.rb"].each { |rb| require(rb) } }
+end
 
 require 'minitest/autorun'
 require 'minitest/pride'
