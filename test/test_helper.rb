00e285c2aa517770f49b12092ebc63ae935e519b
Anthony Garcia <polypressure@outlook.com>
2015-07-26 18:34:27 -0500

    Remove SimpleCov from test_helper, directly use Coveralls.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,5 @@
-require 'simplecov'
 require 'coveralls'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-SimpleCov.start do
-  add_filter "/test/"
-end
-
+Coveralls.wear!
 
 require 'minitest/autorun'
 require 'minitest/pride'
